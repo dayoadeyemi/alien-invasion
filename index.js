a7ea4764c336ef420d5e482ef1f2c5f16fe81348
dayoadeyemi <thedayoadeyemi@gmail.com>
2017-08-13 14:45:23 +0100

    optimized

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const ramda_1 = require("ramda");
-const cityList = fs_1.readFileSync('./world_map_small.txt', 'utf8')
+const cityList = fs_1.readFileSync('./world_map_medium.txt', 'utf8')
     .trim()
     .split('\n')
     .map(ramda_1.split(' '))
-    .map(([city, ...roads]) => [city, {
-        aliens: [],
+    .map(([name, ...roads]) => [name, {
+        name,
         roads: ramda_1.fromPairs(roads.map(ramda_1.split('=')))
     }]);
 const NUMBER_OF_ALIENS = parseInt(process.argv[2]);
@@ -15,61 +15,87 @@ if (isNaN(NUMBER_OF_ALIENS)) {
     throw new Error('first argument must be the initial number of aliens');
 }
 const NUMBER_OF_CITIES = cityList.length;
-const initialState = ramda_1.fromPairs(cityList);
+const worldState = ramda_1.fromPairs(cityList);
 const alienList = ramda_1.times(n => ({
     city: cityList[Math.floor(Math.random() * cityList.length)][1],
     toString() { return 'alien ' + n; }
 }), NUMBER_OF_ALIENS);
-const step = (worldState) => {
-    // 'move' the aliens
-    alienList.forEach(alien => {
-        if (alien.city) {
-            alien.city.aliens.push(alien);
-        }
-    });
-    // resolve destruction
-    for (let city in worldState) {
-        const { aliens, roads } = worldState[city];
-        // assuming here that if >2 aliens are in a city they are all destroyed
-        if (aliens.length > 1) {
-            console.log(`${city} has been destroyed by ` +
-                `${ramda_1.init(aliens).join(', ')} and ${ramda_1.last(aliens)}!`);
-            // clear city and connected roads assuming every road out from a
-            // city has exactly 1 opposite inroad (i.e. roads are straight)
-            for (let road in roads) {
-                const inRoad = {
-                    north: 'south',
-                    east: 'west',
-                    south: 'north',
-                    west: 'east'
-                }[road];
-                const neighbourCityState = worldState[roads[road]];
-                delete neighbourCityState.roads[inRoad];
+/**
+ * clear city and connected roads assuming every road out from a
+ * city has exactly 1 opposite inroad (i.e. roads are straight)
+ * @param cityState
+ */
+const destroyCity = (cityState) => {
+    const { roads } = cityState;
+    for (let road in roads) {
+        const inRoad = {
+            north: 'south',
+            east: 'west',
+            south: 'north',
+            west: 'east'
+        }[road];
+        const neighbourCityState = worldState[roads[road]];
+        delete neighbourCityState.roads[inRoad];
+    }
+    delete worldState[cityState.name];
+};
+/**
+ * Main iteration step. Assumes aliens *must* move to a neighbouring city if
+ * they can
+ *
+ * In general,  #aliens < #cities (as otherwise the aliens will just
+ * kill each other, and in next step #aliens < #cities) so prefer to iterate
+ * over aliens
+ */
+const step = () => {
+    const aliensByCity = new Map();
+    const citiesToDestroy = [];
+    // calculate destruction
+    for (let i = alienList.length; i--;) {
+        const alien = alienList[i];
+        const aliens = aliensByCity.get(alien.city);
+        if (aliens) {
+            aliens.push(alien);
+            // there are already aliens in this city so this alien will be
+            // killed by it
+            alienList.splice(i, 1);
+            // make sure the first alien dies and the city is destroyed
+            if (aliens.length === 2) {
+                citiesToDestroy.push(alien.city);
+                alienList.splice(alienList.indexOf(aliens[0]), 1);
             }
-            cityList.splice(cityList.findIndex($ => $[1] === worldState[city]), 1);
-            delete worldState[city];
-            aliens.forEach(alien => alienList.splice(alienList.indexOf(alien), 1));
         }
         else {
-            // clear local alien list for re-calculating
-            worldState[city].aliens = [];
+            aliensByCity.set(alien.city, [alien]);
         }
     }
+    ;
+    // destroy cities
+    for (let city of citiesToDestroy) {
+        const aliens = aliensByCity.get(city);
+        console.log(`${city.name} has been destroyed by ` +
+            `${ramda_1.init(aliens).join(', ')} and ${ramda_1.last(aliens)}!`);
+        destroyCity(city);
+    }
     // calculate movement
-    alienList.forEach(alien => {
-        const neighbouringCities = Object.keys(alien.city.roads)
-            .map(road => worldState[alien.city.roads[road]]);
-        alien.city = neighbouringCities[Math.floor(Math.random() * neighbouringCities.length)];
-    });
-    let i = alienList.length;
-    while (i--)
-        if (!alienList[i].city)
+    for (let i = alienList.length; i--;) {
+        const neighbouringCities = Object.keys(alienList[i].city.roads)
+            .map(road => worldState[alienList[i].city.roads[road]]);
+        // if there is no neighbouring city the alien is trapped!
+        // no need to process is any more
+        if (neighbouringCities.length === 0) {
             alienList.splice(i, 1);
+            continue;
+        }
+        // calculate movement
+        alienList[i].city = neighbouringCities[Math.floor(Math.random() * neighbouringCities.length)];
+    }
 };
-for (let iterations = 0; iterations < 10000 && alienList.length; iterations++) {
-    step(initialState);
-}
+// Run until all the aliens have been destroyed, or each alien has moved at
+// least 10,000 times
+for (let iterations = 0; iterations < 10000 && alienList.length; iterations++)
+    step();
 // print out the remaining world
-cityList.forEach(([city, cityState]) => {
-    console.log(city, ...ramda_1.toPairs(cityState.roads).map(ramda_1.join('=')));
-});
+for (let city in worldState) {
+    console.log(city, ...ramda_1.toPairs(worldState[city].roads).map(ramda_1.join('=')));
+}
--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs'
 import { split, merge, times, fromPairs, init, last, toPairs, join } from 'ramda'
 
 interface CityState {
-    aliens: { city: CityState }[],
+    name: string
     roads: { north?: string, east?: string, south?: string, west?: string, }
 }
 
@@ -11,8 +11,8 @@ const cityList = readFileSync('./world_map_medium.txt', 'utf8')
     .split('\n')
     //assume city names dont contain spaces
     .map(split(' '))
-    .map(([city, ...roads])=> [city, {
-        aliens: [],
+    .map(([name, ...roads])=> [name, {
+        name,
         roads: fromPairs(roads.map(split('=')) as [string, string][])
     }] as [string, CityState])
 
@@ -24,72 +24,102 @@ if (isNaN(NUMBER_OF_ALIENS)) {
 
 const NUMBER_OF_CITIES = cityList.length
 
-const initialState = fromPairs(cityList)
-type WorldState = typeof initialState
+const worldState = fromPairs(cityList)
 
 const alienList = times(n => ({
     city: cityList[Math.floor(Math.random()*cityList.length)][1],
     toString(){ return 'alien ' + n; }
 }), NUMBER_OF_ALIENS)
 
+/**
+ * clear city and connected roads assuming every road out from a
+ * city has exactly 1 opposite inroad (i.e. roads are straight)
+ * @param cityState 
+ */
+const destroyCity = (cityState: CityState) => {
+    const { roads } = cityState
 
-const step = (worldState: WorldState) => {
-    // 'move' the aliens
-    alienList.forEach(alien => {
-        if (alien.city) {
-            alien.city.aliens.push(alien)
-        }
-    });
-    // resolve destruction
-    for (let city in worldState){
-        const { aliens, roads } = worldState[city]
-        
-        // assuming here that if >2 aliens are in a city they are all destroyed
-        if (aliens.length > 1) {
-            console.log(`${city} has been destroyed by ` +
-            `${init(aliens).join(', ')} and ${last(aliens)}!`);
-
-            // clear city and connected roads assuming every road out from a
-            // city has exactly 1 opposite inroad (i.e. roads are straight)
-            for (let road in roads){
-                const inRoad = {
-                    north: 'south',
-                    east: 'west',
-                    south: 'north',
-                    west: 'east'
-                }[road]
-                const neighbourCityState = worldState[roads[road]]
-                delete neighbourCityState.roads[inRoad]
-            }
-            cityList.splice(cityList.findIndex($ =>
-                $[1] === worldState[city]), 1)
-            delete worldState[city]
-
-            aliens.forEach(alien =>
-                alienList.splice(alienList.indexOf(alien), 1))
-        } else {
-            // clear local alien list for re-calculating
-            worldState[city].aliens = []
-        }
+    for (let road in roads){
+        const inRoad = {
+            north: 'south',
+            east: 'west',
+            south: 'north',
+            west: 'east'
+        }[road]
+        const neighbourCityState = worldState[roads[road]]
+        delete neighbourCityState.roads[inRoad]
     }
+    delete worldState[cityState.name]
+}
+
+/**
+ * Main iteration step. Assumes aliens *must* move to a neighbouring city if 
+ * they can
+ * 
+ * In general,  #aliens < #cities (as otherwise the aliens will just
+ * kill each other, and in next step #aliens < #cities) so prefer to iterate
+ * over aliens
+ */
+const step = () => {
+    const aliensByCity = new Map<CityState, { city: CityState }[]>()
+
+    const citiesToDestroy = [] as CityState[]
+
+    // calculate destruction
+    for (let i = alienList.length; i--;) {
+        const alien = alienList[i]
+        const aliens = aliensByCity.get(alien.city)
+        if (aliens) {
+            aliens.push(alien)
+            // there are already aliens in this city so this alien will be
+            // killed by it
+            alienList.splice(i, 1)
+
+            // make sure the first alien dies and the city is destroyed
+            if (aliens.length === 2) {
+                citiesToDestroy.push(alien.city)
+                alienList.splice(alienList.indexOf(aliens[0]), 1)
+            }
+        }
+        else {
+            aliensByCity.set(alien.city, [alien])
+        }
+    };
+
+    // destroy cities
+    for (let city of citiesToDestroy){
+        const aliens = aliensByCity.get(city)
+        console.log(`${city.name} has been destroyed by ` +
+        `${init(aliens).join(', ')} and ${last(aliens)}!`);
+
+        destroyCity(city)
+    }
+
     // calculate movement
-    alienList.forEach(alien => {
-        const neighbouringCities = Object.keys(alien.city.roads)
-            .map(road => worldState[alien.city.roads[road]])
-            
-        alien.city = neighbouringCities[
+    for (let i = alienList.length; i--;) {
+        
+        const neighbouringCities = Object.keys(alienList[i].city.roads)
+            .map(road => worldState[alienList[i].city.roads[road]])
+        
+        // if there is no neighbouring city the alien is trapped!
+        // no need to process is any more
+        if (neighbouringCities.length === 0) {
+            alienList.splice(i, 1)
+            continue
+        }
+
+        // calculate movement
+        alienList[i].city = neighbouringCities[
             Math.floor(Math.random()*neighbouringCities.length)
         ]
-    })
-    let i = alienList.length
-    while (i--) if (!alienList[i].city) alienList.splice(i, 1)
+    }
 }
 
-for (let iterations = 0; iterations < 10000 && alienList.length; iterations++) {
-    step(initialState)
-}
+// Run until all the aliens have been destroyed, or each alien has moved at
+// least 10,000 times
+for (let iterations = 0; iterations < 10000 && alienList.length; iterations++) step()
 
 // print out the remaining world
-cityList.forEach(([city, cityState]) => {
-    console.log(city, ...toPairs(cityState.roads).map(join('=')))
-})
+for (let city in worldState) {
+    console.log(city, ...toPairs(worldState[city].roads).map(join('=')))
+}
